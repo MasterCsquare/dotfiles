54d3f1968f9f020c028f342e38a24796bc157126
steiner <yasteiner@protonmail.com>
2021-03-25 21:49:06 +0800

    xmonad: remove redundant bracket

--- xmonad.hs
+++ xmonad.hs
@@ -43,7 +43,7 @@ addNETSupported x   = withDisplay $ \dpy -> do
     a_NET_SUPPORTED <- getAtom "_NET_SUPPORTED"
     a               <- getAtom "ATOM"
     liftIO $ do
-       sup <- (join . maybeToList) <$> getWindowProperty32 dpy a_NET_SUPPORTED r
+       sup <- join . maybeToList <$> getWindowProperty32 dpy a_NET_SUPPORTED r
        when (fromIntegral x `notElem` sup) $
          changeProperty32 dpy r a_NET_SUPPORTED a propModeAppend [fromIntegral x]
 
